3c971b2f81350277186576f618439de995318474
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-05 11:27:01 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Source/Library.rs
+++ Source/Library.rs
@@ -1,37 +1,56 @@
-#![allow(non_snake_case)]
+use codeeditorland_echo::{Action, ActionResult, Job, Work, Worker, Yell};
 
-use serde::{Deserialize, Serialize};
-use tokio::{fs::File, io::AsyncWriteExt, net::TcpListener};
+use futures::future::join_all;
+use std::sync::Arc;
+use tokio::{net::TcpListener, sync::mpsc};
+use tokio_tungstenite::accept_async;
 
-#[derive(Serialize, Deserialize)]
-struct FileEdit {
-	Path: String,
-	Content: String,
+struct Worker;
+
+#[async_trait::async_trait]
+impl Worker for Worker {
+	async fn Receive(&self, Action: Action) -> ActionResult {
+		Box::pin(async move {
+			match Action {
+				Action::Write { Path, Content } => match tokio::fs::write(&Path, &Content).await {
+					Ok(_) => {
+						ActionResult { Action, ActionResult: Ok(format!("File written: {}", Path)) }
+					}
+					Err(Error) => ActionResult {
+						Action,
+						ActionResult: Err(format!("Failed to write file: {}", Error)),
+					},
+				},
+				_ => ActionResult {
+					Action,
+					ActionResult: Err("Invalid Action for Worker".to_string()),
+				},
+			}
+		})
+	}
 }
 
 #[tokio::main]
 async fn main() {
-	while let Ok((Stream, _)) = TcpListener::bind("127.0.0.1:9999").await.unwrap().accept().await {
-		tokio::spawn(Handler(Stream));
+	let Work = Arc::new(Work::new());
+	let (tx, rx) = mpsc::channel(100);
+
+	let num_workers = 4;
+	let workers: Vec<_> = (0..num_workers)
+		.map(|_| {
+			let worker = Arc::new(Worker) as Arc<dyn Worker>;
+			tokio::spawn(Job(worker, Work.clone(), tx.clone()))
+		})
+		.collect();
+
+	while let Ok((stream, _)) =
+		TcpListener::bind("127.0.0.1:8080").await.expect("Cannot TcpListener.").accept().await
+	{
+		let queue = Work.clone();
+		let rx = rx.clone();
+		let ws_stream = accept_async(stream).await.expect("Failed to accept");
+		tokio::spawn(Yell(ws_stream, queue, rx));
 	}
-}
 
-async fn Handler(Stream: tokio::net::TcpStream) {
-	let (Read, Write) = tokio_tungstenite::accept_async(Stream).await.unwrap().get_ref().split();
-
-	Read.for_each(|Message| async {
-		if let Ok(Message) = Message {
-			if Message.is_text() {
-				let Edit: FileEdit = serde_json::from_str(Message.to_text().unwrap()).unwrap();
-
-				File::create(Edit.Path)
-					.await
-					.unwrap()
-					.write_all(Edit.Content.as_bytes())
-					.await
-					.unwrap();
-			}
-		}
-	})
-	.await;
+	join_all(workers).await;
 }
